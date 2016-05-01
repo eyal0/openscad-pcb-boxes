f2afbf5490d6b76ee3270b745c9429b779cad6d8
Eyal Soha <eyalsoha@google.com>
2016-05-01 20:38:28 +0300

    WIP, trying to debug facet triangulation

--- src/tools/stl_sorter.py
+++ src/tools/stl_sorter.py
@@ -12,31 +12,34 @@ else:
   solid = stl.read_binary_string(in_stl)
 
 
+new_facets = []
 for i in range(len(solid.facets)):
   facet = solid.facets[i]
   f_new = stl.Facet(None, [stl.Vector3d(*(round(coord,3) for coord in vertex))
                        for vertex in facet])
   f_new.recalculate_normal()
-  if f_new.normal == None:
-    print(f_new)
+  if f_new.normal != None:
+    new_facets.append(f_new)
+solid.facets = new_facets
 
-  solid.facets[i] = f_new
+count = 0
+while count < 10000:
+  joined_facet = solid.remove_planar_edge()
+  if not joined_facet:
+    break
+  sys.stderr.write("joining a facet %d\n" % count)
+  count += 1
+  while joined_facet.remove_1d_vertex():
+    #joined_facet.remove_colinear_vertex()):
+    sys.stderr.write("removing a vertex\n")
+    pass
+
+new_f = []
+for f in solid.facets:
+  new_f += f.split_to_triangles()
+solid = stl.Solid(solid.name, new_f)
 solid.sort_facets()
-#count = 0
-#while solid.remove_planar_edge() and count < 30:
-#  count += 1
-#  sys.stderr.write("facets down to %d\n" % (count))
-#new_f = []
-#for f in solid.facets:
-#  new_f += f.split_to_triangles()
-#solid = stl.Solid(solid.name, new_f)
-#solid.sort_facets()
 
-#for facet in solid.facets:
-#  for vertex in enumerate(facet.vertices):
-#    facet[vertex[0]] = (round(vertex[1].x),
-#                        round(vertex[1].y),
-#                        round(vertex[1].z))
 if infile_ascii:
   solid.write_ascii(sys.stdout)
 else:
