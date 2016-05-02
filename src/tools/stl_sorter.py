a8adb621052a22d830750b0a91887750816def22
Eyal Soha <eyalsoha@gmail.com>
2016-05-02 21:08:39 +0300

    original code and broken

--- src/tools/stl_sorter.py
+++ src/tools/stl_sorter.py
@@ -25,32 +25,32 @@ else:
 for i in range(len(solid.facets)):
   solid.facets[i].recalculate_normal()
 
-count = 0
-while count < 10000:
-  joined_facet = solid.remove_planar_edge()
-  if not joined_facet:
-    break
-  sys.stderr.write("joining a facet: %s\n" % joined_facet)
-  count += 1
-  while joined_facet.remove_1d_vertex():
-    #joined_facet.remove_colinear_vertex()):
-    sys.stderr.write("removing a vertex: %s\n" % joined_facet.vertices)
-    if len(joined_facet.vertices) < 3:
-      # Become degenerate, remove.
-      solid.facets.remove(joined_facet)
-      break
-    pass
+#count = 0
+#while count < 10000:
+#  joined_facet = solid.remove_planar_edge()
+#  if not joined_facet:
+#    break
+#  sys.stderr.write("joining a facet: %s\n" % joined_facet)
+#  count += 1
+#  while joined_facet.remove_1d_vertex():
+#    #joined_facet.remove_colinear_vertex()):
+#    sys.stderr.write("removing a vertex: %s\n" % joined_facet.vertices)
+#    if len(joined_facet.vertices) < 3:
+#      # Become degenerate, remove.
+#      solid.facets.remove(joined_facet)
+#      break
+#    pass
 
-new_f = []
-for f in solid.facets:
-  new_triangles = f.split_to_triangles()
-  for t in new_triangles:
-    t.recalculate_normal()
-    sys.stderr.write("removing: %s\n" % t)
-    #if not t.normal:
-    #  new_triangles.remove(t)
-  new_f += new_triangles
-solid = stl.Solid(solid.name, new_f)
+#new_f = []
+#for f in solid.facets:
+#  new_triangles = f.split_to_triangles()
+#  for t in new_triangles:
+#    t.recalculate_normal()
+#    sys.stderr.write("removing: %s\n" % t)
+#    #if not t.normal:
+#    #  new_triangles.remove(t)
+#  new_f += new_triangles
+#solid = stl.Solid(solid.name, new_f)
 solid.sort_facets()
 
 if infile_ascii:
